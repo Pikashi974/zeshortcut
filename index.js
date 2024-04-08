3b4fa8181f29a06da1a416790d4e197d68fd5a85
Jérémy PAYET <the_big_boss97437@hotmail.fr>
2024-04-08 13:51:08 +0400

    Ajout mail + repas

--- index.js
+++ index.js
@@ -4,6 +4,17 @@ const port = 5001;
 const path = require("node:path");
 const fs = require("fs");
 const bodyParser = require("body-parser");
+const nodemailer = require("nodemailer");
+
+const transporter = nodemailer.createTransport({
+  host: "smtp.mailtrap.io",
+  port: 587,
+  secure: false, // Use `true` for port 465, `false` for all other ports
+  auth: {
+    user: "",
+    pass: "",
+  },
+});
 
 app.use(express.json());
 app.use("/aliment", express.static(__dirname + "src/aliment"));
@@ -17,10 +28,37 @@ app.get("/aliment", async (req, res) => {
   var data = require("./src/aliment/aliments.json");
   res.send(data);
 });
+app.get("/repas", async (req, res) => {
+  var data = require("./src/aliment/repas.json");
+  res.send(data);
+});
 app.get("/src/js/index.js", function (req, res) {
   res.sendFile(path.join(__dirname + "/src/js/index.js"));
 });
-
+app.get("/repas/maigrir", async (req, res) => {
+  var data = require("./src/aliment/repas.json");
+  res.send(
+    data.filter(
+      (repas) =>
+        calcCalorieRepas(repas) <= req.body.calories &&
+        calcGlucidesRepas(repas) <= req.body.glucides &&
+        calcLipidesRepas(repas) <= req.body.lipides &&
+        calcProteinesRepas(repas) <= req.body.proteines
+    )
+  );
+});
+app.get("/repas/grossir", async (req, res) => {
+  var data = require("./src/aliment/repas.json");
+  res.send(
+    data.filter(
+      (repas) =>
+        calcCalorieRepas(repas) >= req.body.calories &&
+        calcGlucidesRepas(repas) >= req.body.glucides &&
+        calcLipidesRepas(repas) >= req.body.lipides &&
+        calcProteinesRepas(repas) >= req.body.proteines
+    )
+  );
+});
 app.post("/aliment/add", function (req, res) {
   let message = "Echec de l'envoi";
   try {
@@ -39,6 +77,57 @@ app.post("/aliment/add", function (req, res) {
 //   res.send(data);
 // });
 
+app.post("/sendMail", async function (req, res) {
+  const info = await transporter.sendMail({
+    from: '"Maddison Foo Koch 👻" <maddison53@ethereal.email>', // sender address
+    to: req.body.mail, // list of receivers
+    subject: "Hello ✔", // Subject line
+    text: "Hello world?", // plain text body
+    html: "<b>Hello world?</b>", // html body
+  });
+  res.send(info);
+});
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
+function calcCalorieRepas(repas) {
+  var data = require("./src/aliment/aliments.json");
+  let ingredients = Object.keys(repas.recette);
+  let totalCal = 0;
+  ingredients.forEach((element) => {
+    let aliment = data.filter((obj) => obj.nom == element)[0];
+    totalCal += aliment.calories;
+  });
+  return totalCal;
+}
+function calcLipidesRepas(repas) {
+  var data = require("./src/aliment/aliments.json");
+  let ingredients = Object.keys(repas.recette);
+  let totalCal = 0;
+  ingredients.forEach((element) => {
+    let aliment = data.filter((obj) => obj.nom == element)[0];
+    totalCal += aliment.lipides;
+  });
+  return totalCal;
+}
+function calcGlucidesRepas(repas) {
+  var data = require("./src/aliment/aliments.json");
+  let ingredients = Object.keys(repas.recette);
+  let totalCal = 0;
+  ingredients.forEach((element) => {
+    let aliment = data.filter((obj) => obj.nom == element)[0];
+    totalCal += aliment.glucides;
+  });
+  return totalCal;
+}
+function calcProteinesRepas(repas) {
+  var data = require("./src/aliment/aliments.json");
+  let ingredients = Object.keys(repas.recette);
+  let totalCal = 0;
+  ingredients.forEach((element) => {
+    let aliment = data.filter((obj) => obj.nom == element)[0];
+    totalCal += aliment.proteines;
+  });
+  return totalCal;
+}
